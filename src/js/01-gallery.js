9d7255c6cc9ff8f1da6e6d26179e4c9c627582e7
Sudnikevych <sudnikevych@gmail.com>
2022-10-25 17:20:18 +0300

    add HW8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,32 +1,5 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
 import { galleryItems } from './gallery-items';
+// Change code below this line
 
-// console.log(galleryItems);
-
-const gallery = document.querySelector('.gallery');
-
-const galleryImg = galleryItems
-  .map(
-    items =>
-      `<div class="gallery__item">
-    <a class="gallery__link" href="${items.original}">
-        <img
-            class="gallery__image"
-            src="${items.preview}"
-            alt="${items.description}"
-        />
-    </a>
-</div>`
-  )
-  .join('');
-
-gallery.innerHTML = galleryImg;
-
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionPosition: 'bottom',
-  captionDelay: 250,
-  captionsData: 'alt',
-});
-
-lightbox.on('show.simplelightbox');
+console.log(galleryItems);
