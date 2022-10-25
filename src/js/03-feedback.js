3e70fe9c9b2ea2d15cfe137a2e35b12b8106aad9
Sudnikevych <sudnikevych@gmail.com>
2022-10-25 17:27:38 +0300

    add 01, 02, 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+//const inputRef = document.querySelector('input');
+//const textareaRef = document.querySelector('textarea');
+
+const datesForm = {};
+
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+
+addContentForm();
+
+function onFormInput(evt) {
+  datesForm[evt.target.name] = evt.target.value;
+
+  save(datesForm);
+}
+
+function save(object) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(object));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
+
+function addContentForm() {
+  const valueKey = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (valueKey) {
+    const parsValueKey = JSON.parse(valueKey);
+
+    Object.entries(parsValueKey).forEach(([name, value]) => {
+      datesFormForLocalStor[name] = value;
+      formRef.elements[name].value = value;
+    });
+  }
+}
