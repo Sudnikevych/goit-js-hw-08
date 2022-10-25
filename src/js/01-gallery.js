b57439df3bb3bac0b12391fed9b80c1118343959
Sudnikevych <sudnikevych@gmail.com>
2022-10-25 16:58:23 +0300

    add 01, 02, 03

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryImg = galleryItems
+  .map(
+    items =>
+      `<div class="gallery__item">
+    <a class="gallery__link" href="${items.original}">
+        <img
+            class="gallery__image"
+            src="${items.preview}"
+            alt="${items.description}"
+        />
+    </a>
+</div>`
+  )
+  .join('');
+
+gallery.innerHTML = galleryImg;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+lightbox.on('show.simplelightbox');
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+//const inputRef = document.querySelector('input');
+//const textareaRef = document.querySelector('textarea');
+
+const datesFormForLocalStor = {};
+
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+
+addContentForm();
+
+function onFormInput(evt) {
+  datesFormForLocalStor[evt.target.name] = evt.target.value;
+
+  save(datesFormForLocalStor);
+}
+
+function save(object) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(object));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
+
+function addContentForm() {
+  const valueKey = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (valueKey) {
+    const parseValueKey = JSON.parse(valueKey);
+
+    Object.entries(parseValueKey).forEach(([name, value]) => {
+      datesFormForLocalStor[name] = value;
+      formRef.elements[name].value = value;
+    });
+  }
+}
